be44ce166b9727dfc83da129e6f4818b0ecf0908
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-07-29 21:57:24 +0200

    docs(meta): :memo: update title and description for better SERP snippet optimization

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { Config } from './types'
 export const configSite: Config = {
   title: 'KiraKana',
   description:
-    'Domina los silabarios japoneses Hiragana y Katakana a través de nuestra plataforma interactiva de aprendizaje. Practica cada kana con un puzzle intuitivo diseñado para principiantes que desean memorizar estos sistemas de escritura esenciales del japonés.',
+    'Domina los silabarios japoneses Hiragana y Katakana a través de nuestra plataforma interactiva. Practica cada kana con un puzzle intuitivo.',
   lang: 'es',
   author: 'bmmedina99',
   url: 'https://kirakana.app',
