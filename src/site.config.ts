f54f15e90b58879d99b78e430fb4ba0d5064b969
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-08-09 01:22:38 +0200

    refactor(config): standardize export names for kana arrays

--- src/site.config.ts
+++ src/site.config.ts
@@ -28,10 +28,18 @@ export const navLinks = [
   },
 ]
 
-export const HIRAGANA: KanaItem[] = [
+export const Hiragana: KanaItem[] = [
   { kana: 'あ', romanji: 'a' },
   { kana: 'い', romanji: 'i' },
   { kana: 'う', romanji: 'u' },
   { kana: 'え', romanji: 'e' },
   { kana: 'お', romanji: 'o' },
+]
+
+export const Katakana: KanaItem[] = [
+  { kana: 'ア', romanji: 'a' },
+  { kana: 'イ', romanji: 'i' },
+  { kana: 'ウ', romanji: 'u' },
+  { kana: 'エ', romanji: 'e' },
+  { kana: 'オ', romanji: 'o' },
 ]
