5f8c037c05f433bf5414cbacde5eef2817b7ce3b
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2026-01-11 03:04:25 +0100

    refactor(workcards): restructure color property to use object for background and text

--- src/libs/constants/workcards.ts
+++ src/libs/constants/workcards.ts
@@ -1,28 +1,40 @@
 export const WORK_CARDS = [
   {
     icon: 'learn',
-    color: 'text-red-900',
+    color: {
+      background: 'from-red-100 via-red-200 to-red-300',
+      text: 'text-red-900',
+    },
     title: '1. Aprende',
     description:
       'Descubre cada carácter con guías visuales, pronunciación clara y ejemplos prácticos.',
   },
   {
     icon: 'practice',
-    color: 'text-blue-900',
+    color: {
+      background: 'from-blue-100 via-blue-200 to-blue-300',
+      text: 'text-blue-900',
+    },
     title: '2. Practica',
     description:
       'Refuerza tu aprendizaje con ejercicios interactivos de escritura y reconocimiento.',
   },
   {
     icon: 'challenge',
-    color: 'text-amber-900',
+    color: {
+      background: 'from-amber-100 via-amber-200 to-amber-300',
+      text: 'text-amber-900',
+    },
     title: '3. Desafíate',
     description:
       'Desafía tus habilidades con acertijos que se enfocan en tus aspectos a perfeccionar.',
   },
   {
     icon: 'progress',
-    color: 'text-red-900',
+    color: {
+      background: 'from-red-100 via-red-200 to-red-300',
+      text: 'text-red-900',
+    },
     title: '4. Progresa',
     description:
       'Sigue tu avance con estadísticas detalladas y comentarios individualizados.',
