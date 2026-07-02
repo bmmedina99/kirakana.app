924afc9c3b5d6a5465049f8fc8bf0563172a5d53
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2026-07-02 23:05:25 +0200

    fix(data): remove outdated comments

--- src/features/data/hiragana.ts
+++ src/features/data/hiragana.ts
@@ -1,5 +1,3 @@
-// src/features/kana/data/hiragana.ts
-
 import {
   createKanaGroup,
   getKanaItemsFromGroups,
--- src/features/data/katakana.ts
+++ src/features/data/katakana.ts
@@ -1,5 +1,3 @@
-// src/features/kana/data/katakana.ts
-
 import {
   createKanaGroup,
   getKanaItemsFromGroups,
