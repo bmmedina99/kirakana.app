023b713431219e97e2d9cc2dba7d522072ba4215
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2026-08-30 00:56:31 +0200

    fix: update image paths to use assets directory

--- src/lib/schema/modules/base.schema.ts
+++ src/lib/schema/modules/base.schema.ts
@@ -8,7 +8,7 @@ export const organizationSchema: SchemaBase = {
   url: configSite.url,
   logo: {
     '@type': 'ImageObject',
-    url: `${configSite.url}/images/logo.webp`,
+    url: `${configSite.url}/assets/images/logo.webp`,
   },
 }
 
